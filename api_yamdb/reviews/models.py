7475f86564b3b94e6398a77b8fffb6cd5898891c
Александр Осипов <a.b.osipov@gmail.com>
2023-02-15 14:18:27 +0300

    add user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+ROLE_SET = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+
+
+class YaMdbUser(AbstractUser):
+    """Переопределенная можель пользователя."""
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        'Уровень доступа',
+        choices=ROLE_SET,
+        default='user',
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
