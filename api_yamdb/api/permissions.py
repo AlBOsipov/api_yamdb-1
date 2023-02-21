bfe03b2eea69ade8b89fb19a7128509c7bbd555c
Александр Осипов <a.b.osipov@gmail.com>
2023-02-21 18:02:37 +0300

    Добавил еднпоинт /me и /user, почти прошел тест 00 и 01

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,58 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+        )
+
+
+class UserPermission(BasePermission):
+    """Проверка, что пользователь является модератором."""
+    def has_permission(self, request, view):
+        return request.user.role == 'user'
+
+
+class ModeratorPermission(BasePermission):
+    """Проверка, что пользователь является модератором."""
+    def has_permission(self, request, view):
+        return request.user.role == 'moderator'
+
+
+class AdminPermission(BasePermission):
+    """Проверка, что пользователь является модератором."""
+    def has_permission(self, request, view):
+        return request.user.role == 'admin' or request.user.is_superuser
+
+
+class SuperPermission(BasePermission):
+    """Проверка, что пользователь является модератором."""
+    def has_permission(self, request, view):
+        return request.user.role == request.user.is_superuser
+
+
+class AuthorOrAdmin(permissions.BasePermission):
+    """Разрешение доступа к /me/ автору и админу."""
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (
+            request.user.role == 'admin'
+            or obj.user.username == request.user.username
+            # или or obj.user == request.user
+            # если это не сработает, обращайся через request.path_info
+        )
+
+
+class RoleOrReadOnly(permissions.BasePermission):
+    """Разрешение доступа автору, админу, модератору."""
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'moderator'
+            or request.user.role == 'admin'
+            or obj.author == request.user
+        )
--- api_yamdb/reviews/services.py
+++ api_yamdb/reviews/services.py
@@ -1,7 +1,14 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 
 def validate_name_me(name):
 
     if name.lower() == "me":
         raise ValidationError('Имя не должно быть me')
+
+    validator = RegexValidator(
+        regex=r'^[\w.@+-]+$',
+        message='Некорректный slug.'
+    )
+    validator(name)
