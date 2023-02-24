75e555095fe080dd3645a8df6dfe0ed8da8c7091
Александр Осипов <a.b.osipov@gmail.com>
2023-02-24 13:15:36 +0300

    fix user permission

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import status, mixins, filters, viewsets
-from rest_framework.decorators import action, permission_classes
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import (
